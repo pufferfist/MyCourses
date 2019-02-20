cd9d03c46a7e10d1314b2069ef7b5f8e1c579ae7
赶紧抬走 <840326461@qq.com>
2019-02-20 11:17:46 +0800

    init

---
+++ src/main/java/nju/mikasa/mycourses/MycoursesApplication.java
@@ -0,0 +1,13 @@
+package nju.mikasa.mycourses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycoursesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MycoursesApplication.class, args);
+    }
+
+}
